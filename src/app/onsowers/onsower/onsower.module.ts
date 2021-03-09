afb0fe8cafe89c69cfaf9ed61c37448fcdcc378b
jmg <jmgrino@gmail.com>
2021-03-09 18:13:48 +0100

    Move EditBanner component to shared folder

--- src/app/onsowers/onsower/onsower.module.ts
+++ src/app/onsowers/onsower/onsower.module.ts
@@ -1,7 +1,7 @@
 import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { OnsowerComponent } from './onsower.component';
-import { EditBannerComponent } from '../edit-banner/edit-banner.component';
+import { EditBannerComponent } from '../../shared/edit-banner/edit-banner.component';
 import { IonicModule } from '@ionic/angular';
 
 
