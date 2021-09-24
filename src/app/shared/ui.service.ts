8a768b092833cd6764a13e71822d187a58801383
jmg <jmgrino@gmail.com>
2021-09-24 19:03:38 +0200

    LL20210924 Some errors solved

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -113,7 +113,7 @@ export class UIService {
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
 
-    return !!(control && control.invalid && control.touched);
+    return !!(control && control.invalid);
     // return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
